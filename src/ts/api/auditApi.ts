3c9f08891e0a452bdba252c94ea51c76551ab978
AsaelCRdev <166563204+AsaelCRdev@users.noreply.github.com>
2026-08-10 14:25:06 -0400

    Delete src directory

--- src/ts/api/auditApi.ts
+++
@@ -1,7 +0,0 @@
-import { delay } from './apiClient';
-import { dbGetAuditLogs } from './mockBackend';
-import type { AuditLogEntry } from '../models/auditLog.model';
-
-export async function fetchAuditLogs(): Promise<AuditLogEntry[]> {
-  return delay(dbGetAuditLogs());
-}

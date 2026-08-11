0726cc2d1a1411bb4567becf54762d479a0313ee
Lawliet-cpu <cateran.23@est.ucab.edu.ve>
2026-08-11 13:57:02 -0400

    fix: rediseño de backend

--- backend/src/main/java/com/clearTier/backend/BackendApplication.java
+++ backend/src/main/java/com/clearTier/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.system.backend;
+package com.clearTier.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/clearTier/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/clearTier/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.system.backend;
+package com.clearTier.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
